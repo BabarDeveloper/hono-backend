797df5c71522c3d80f4b57fa600da60874863741
BabarDeveloper <babardeveloper2@gmail.com>
2026-01-16 03:45:50 +0500

    add getUserId function and update routes to include user retrieval

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -49,9 +49,12 @@ export const createUser = async (c: Context) => {
 
     return c.json({ message: "User created successfully", data: newUser }, 201);
   } catch (error) {
-    return c.json({
-      error: "Internal server error",
-    }, 500);
+    return c.json(
+      {
+        error: "Internal server error",
+      },
+      500
+    );
   }
 };
 
@@ -60,4 +63,13 @@ export const getUsers = async (c: Context) => {
     users_count: users.length,
     data: users,
   });
+};
+
+export const getUserId = async (c: Context) => {
+  const id = c.req.query;
+  const user = users.find((u) => u.id === Number(id));
+  if (!user) {
+    return c.json({ error: "User not found" }, 404);
+  }
+  return c.json({ data: user });
 };
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,10 +1,12 @@
 import { Hono } from "hono";
-import { createUser, getUsers } from "../controllers/user.controller.js";
+import { createUser, getUserId, getUsers } from "../controllers/user.controller.js";
 
 const router = new Hono();
 
 router.post("/", createUser);
 
 router.get("/", getUsers);
+
+router.get("/:id", getUserId);
 
 export default router;
