8424270e1512b7e6d9fb3c874f704a13e1e2706f
BabarDeveloper <babardeveloper2@gmail.com>
2026-01-17 01:01:27 +0500

    refactor createUser function by removing commented-out code for clarity

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -2,34 +2,6 @@ import type { Context } from "hono";
 
 let users: any[] = [];
 
-// export const createUser = async (c: Context) => {
-//   try {
-//     const body = await c.req.json();
-//     const { name, email } = body;
-
-//     if (!name || !email) {
-//       return c.json({ error: "Name and Email are required" }, 400);
-//     }
-
-//     const newUser = {
-//       id: Date.now(),
-//       name,
-//       email,
-//     };
-
-//     users.push(newUser);
-
-//     return c.json({ message: "User created successfully", data: newUser }, 201);
-//   } catch (error) {
-//     return c.json(
-//       {
-//         error: "Internal server error",
-//       },
-//       500
-//     );
-//   }
-// };
-
 export const createUser = async (c: Context) => {
   try {
     const body = await c.req.json();
